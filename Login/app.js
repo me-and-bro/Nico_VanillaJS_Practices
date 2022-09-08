f60eab22adaaafc554c10b2d9b1b8eebbc479a40
Dojin Park <djpark1674@gmail.com>
2022-09-08 15:20:41 +0900

    Code Simplification

--- Login/app.js
+++ Login/app.js
@@ -5,22 +5,22 @@ const greeting = document.getElementById("greeting");
 const HIDDEN_CLASSNAME = "hidden";                      
 const USERNAME_KEY = "username";
 
-function handleInputSubmit(event) {                     // "submit" EventListener
-    event.preventDefault();                             // 브라우저 기본 동작 제어
-    const username = loginInput.value;                  // Username 저장
-
-    localStorage.setItem(USERNAME_KEY, username);       // localStorage User 키/값 저장
-
-    loginForm.classList.add(HIDDEN_CLASSNAME);          // Form 숨기기
-    paintGreetings(username);                           // Greeting 표시
+function handleInputSubmit(event) {
+    // "submit" EventListener
+    event.preventDefault();                                     // 브라우저 기본 동작 제어
+    localStorage.setItem(USERNAME_KEY, loginInput.value);       // localStorage User 키/값 저장
+    loginForm.classList.add(HIDDEN_CLASSNAME);                  // Form 숨기기
+    paintGreetings();                                           // Greeting 표시
 }
 
-function paintGreetings(username){                          // Greeting 표시 함수
-    greeting.innerText = `Hello ${username}`;               // innerText 저장
-    greeting.classList.remove(HIDDEN_CLASSNAME);            // Greeting 표시
+function paintGreetings(){
+    // Greeting 표시 함수
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;                   // innerText 저장
+    greeting.classList.remove(HIDDEN_CLASSNAME);                // Greeting 표시
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);   // localStorage username 값 저장
+const savedUsername = localStorage.getItem(USERNAME_KEY);       // localStorage username 값 저장
 
 if (savedUsername === null) {
     // form 표시
@@ -28,5 +28,5 @@ if (savedUsername === null) {
     loginForm.addEventListener("submit", handleInputSubmit);
 } else {
     // greeting 표시
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
